d4161f1a05c6693089e3ecd51031f0730be84211
Светлана <asvetta@gmail.com>
2021-09-15 03:39:56 +0300

    правки

--- source/js/script.js
+++ source/js/script.js
@@ -2,7 +2,7 @@ const mainnavtoggle = document.querySelector(".main-nav--close");
 const nav = document.querySelector(".main-nav");
 const button = document.querySelector(".main-nav__toggle");
 
-nav.classList.add("main-nav--open");
+nav.classList.remove("main-nav--open");
 
 button.addEventListener("click", function () {
   nav.classList.toggle("main-nav--open");
